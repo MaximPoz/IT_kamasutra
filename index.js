b0120af625f5be2ee41396b9cea2f99b33b39968
MaximPoz <executioner2126@gmail.com>
2021-09-24 19:08:07 +0900

    js

---
+++ index.js
@@ -0,0 +1,10 @@
+const Ulitka = () => {
+    let sU = 7-2;
+    let rast = 32;
+    let day = i;
+    for(let i=0; i<rast; i++)
+        if(i==rast){
+            console.log(i)
+            break;
+        }
+}
